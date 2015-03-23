55a0b5823d9a60812eb0c32d2422d1c0cdf5e829
John Meeker <jmeeker516@gmail.com>
2015-03-23 15:55:37 -0700

    Cancel string

--- Pod/Providers/Twitter/SimpleAuthTwitterProvider.m
+++ Pod/Providers/Twitter/SimpleAuthTwitterProvider.m
@@ -85,7 +85,7 @@
                 [sheet addButtonWithTitle:title];
             }
             sheet.actionSheetStyle = UIActionSheetStyleBlackTranslucent;
-            sheet.cancelButtonIndex = [sheet addButtonWithTitle:NSLocalizedString(@"GENERAL_CANCEL", nil)];
+            sheet.cancelButtonIndex = [sheet addButtonWithTitle:NSLocalizedString(@"Cancel", nil)];
             
             SEL s = @selector(actionSheet:clickedButtonAtIndex:);
             Protocol *p = @protocol(UIActionSheetDelegate);
